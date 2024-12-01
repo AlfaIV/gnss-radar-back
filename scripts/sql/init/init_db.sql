ef08faa0e71ac00d195bd2703299f73503c3f0a6
JuFnd <none@localhost>
2024-12-01 03:09:28 +0300

    idx satellite

--- scripts/sql/init/init_db.sql
+++ scripts/sql/init/init_db.sql
@@ -93,6 +93,7 @@ CREATE TABLE IF NOT EXISTS measurements_spectrum (
 );
 
 CREATE UNIQUE INDEX tas_device_id_start_end_satellite_id_signal_type ON tasks (device_id, satellite_id, signal_type, start_at, end_at);
+CREATE UNIQUE INDEX tas_satellite_idx ON satellites (satellite_name, external_satellite_id)
 
 INSERT INTO profile(login, password, role, email, organization_name, first_name, second_name) VALUES ('admin', '\xc7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec', 'ADMIN', 'admin@mail.ru', 'gnss-company', 'admin', 'admin');
 
