204c96e132377fd54efde9b29a78a4c391928aa1
Glibusss <expansifff@gmail.com>
2025-03-02 18:24:16 +0300

    fix

--- gnss-user/internal/domain.go
+++ gnss-user/internal/domain.go
@@ -8,7 +8,6 @@ type UserInfoRequest struct {
 }
 
 type UserInfoResponse struct {
-	Id               string   `json:"id"`
 	Login            string   `json:"login"`
 	Role             string   `json:"role"`
 	OrganizationName string   `json:"organizationName"`
@@ -25,10 +24,25 @@ type CreateUserRequest struct {
 	Surname          string `json:"surname"`
 }
 
+type PaginatedRequest struct {
+	Page uint64
+	Size uint64
+}
+
+type UserForAdmin struct {
+	Name             string `json:"name"`
+	Surname          string `json:"surname"`
+	Email            string `json:"email"`
+	Login            string `json:"login"`
+	OrganizationName string `json:"organizationName"`
+}
+
 type Repository interface {
 	GetUserInfo(ctx context.Context, request UserInfoRequest) (UserInfoResponse, error)
 	CreateUser(ctx context.Context, request CreateUserRequest) error
 	ValidatePermissions(ctx context.Context, userId string, api string) (bool, error)
 	ResolveUserSignUp(ctx context.Context, userLogin string, resolution string) error
 	ChangeUserPermissions(ctx context.Context, userLogin string, userRole string) error
+	GetSignUpRequestions(ctx context.Context, params PaginatedRequest) ([]UserForAdmin, error)
+	GetUserForAdmin(ctx context.Context, params PaginatedRequest) ([]UserForAdmin, error)
 }
--- gnss-user/internal/repository/repository.go
+++ gnss-user/internal/repository/repository.go
@@ -2,10 +2,13 @@ package statistics_repository
 
 import (
 	"context"
+	user_domain "gnss-radar/gnss-user/internal"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type PgxIFace interface {
@@ -22,4 +25,52 @@ type UserRepo struct {
 
 func NewUserRepo(pool PgxIFace, logger *logrus.Logger) *UserRepo {
 	return &UserRepo{pool: pool, logger: logger}
+}
+
+func (ur *UserRepo) GetUserInfo(ctx context.Context, request user_domain.UserInfoRequest) (user_domain.UserInfoResponse, error) {
+    userQuery := `
+        SELECT 
+            password, 
+            login, 
+            email, 
+            first_name, 
+            second_name, 
+            role, 
+            organization_name 
+        FROM user 
+        WHERE login = $1;
+    `
+    var hashedPassword string
+    var UserInfo user_domain.UserInfoResponse
+
+    err := ur.pool.QueryRow(ctx, userQuery, request.Login).Scan(
+        &hashedPassword,
+        &UserInfo.Login,
+        &UserInfo.Email,
+        &UserInfo.Name,
+        &UserInfo.Surname,
+        &UserInfo.Role,
+        &UserInfo.OrganizationName,
+    )
+    if err != nil {
+        return UserInfo, errors.Wrapf(err, "failed to get user info for user %s", request.Login)
+    }
+
+    if err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(request.Password)); err != nil {
+        return UserInfo, errors.Wrapf(err, "failed to validate password for user %s", request.Login)
+    }
+
+    apiQuery := `
+        SELECT COALESCE(array_agg(api), '{}'::text[]) 
+        FROM role_api 
+        WHERE role = $1;
+    `
+    var apis []string
+    if err := ur.pool.QueryRow(ctx, apiQuery, UserInfo.Role).Scan(&apis); err != nil {
+        return UserInfo, errors.Wrapf(err, "failed to get APIs for role %s", UserInfo.Role)
+    }
+    
+    UserInfo.Api = apis
+
+    return UserInfo, nil
 }
