87950147d66ab4215f0743fadc50f6f18a0fa6f8
zlev <zlev@ZLEV>
2025-03-23 10:26:11 +0300

    init

--- gnss-satellites/app/api/v1/endpoints/satellities.py
+++ gnss-satellites/app/api/v1/endpoints/satellities.py
@@ -4,7 +4,10 @@ from fastapi import APIRouter, Depends
 from app.core.container import Container
 from app.schemas.sattelites_position import (
     RadarPositionRequest,
+    RadarPositionGeograthRequest,
     SatellitesPositionResponce,
+    SatellitesTimeResponce,
+    
 )
 from app.services import SatellitesPositions
 
@@ -21,3 +24,11 @@ def post_sattelites(
     service: SatellitesPositions = Depends(Provide[Container.satellite_services]),
 ):
     return service.get_sattelites_positions(radar)
+
+@router.post("/time", response_model=SatellitesTimeResponce)
+@inject
+def post_time_sattelites(
+    radar: RadarPositionGeograthRequest,
+    service: SatellitesPositions = Depends(Provide[Container.satellite_services]),
+):
+    return service.get_sattelites_times_vison(radar)
--- gnss-satellites/app/services/sattelites_positions.py
+++ gnss-satellites/app/services/sattelites_positions.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime
+from datetime import datetime,timedelta
 
 import numpy as np
 from astropy import units as u
@@ -20,6 +20,11 @@ from app.entities.sattellites import TLE, SatellitePosition
 from app.schemas.sattelites_position import (
     RadarPositionRequest,
     SatellitesPositionResponce,
+    SatellitesTimeResponce,
+    RadarPositionGeograthRequest,
+    VisionTime,
+    SatelliteTime
+    
 )
 
 
@@ -77,6 +82,7 @@ class SatellitesPositions:
                     "Group": grouping,
                     "Name": satellite_name,
                     "Azimuth": sattelite_props["Azimuth"],
+                    "Elevation": sattelite_props["Elevation"],
                     "Range": sattelite_props["Range"],
                 }
             )
@@ -148,3 +154,99 @@ class SatellitesPositions:
             "Latitude": round(geo.lat.degree, 2),
             "Height": round(geo.height.to(u.km).value, 2),
         }
+    
+
+    def get_sattelites_times_vison(
+        self, radar: RadarPositionGeograthRequest
+    ) -> SatellitesTimeResponce:
+        current_time = datetime.now()
+        radar_position = {
+            "radar_latitude": radar.radar_latitude,
+            "radar_longitude": radar.radar_longitude,
+            "radar_height": radar.radar_height,
+        }
+
+        satellite_time = []
+
+        for satellite in self.satellites:
+            visibility_times = self.visibility_times(
+                current_time, satellite["satrec"], radar_position
+            )
+            name = satellite["tle"].name.strip()
+            parts = name.split()
+            grouping = parts[0]
+            satellite_name = " ".join(parts[1:])
+            satellite_time.append(
+                SatelliteTime(
+                    Group=grouping,
+                    Name=satellite_name,
+                    Time=visibility_times,
+                )
+            )
+
+        return SatellitesTimeResponce(Satellites=satellite_time)
+
+
+    def visibility_times(
+        self,
+        day: datetime,
+        satellite: object,
+        observer: RadarPositionRequest,
+        min_elevation: float = 0.0,  
+        max_elevation: float = 360.0, 
+        min_azimuth: float = 0.0,   
+        max_azimuth: float = 360.0,
+        time_step: timedelta = timedelta(minutes=10),  # Шаг по времени
+    ) -> list:      
+
+        latitude = observer["radar_latitude"] 
+        longitude = observer["radar_longitude"] 
+        height = observer["radar_height"]
+
+        observer_location = EarthLocation(lat=latitude * u.deg, lon=longitude * u.deg, height=height * u.m)
+
+        visibility_times = []
+        current_time = day.replace(hour=0, minute=0, second=0, microsecond=0)
+        end_time = day.replace(hour=23, minute=59, second=59, microsecond=999999)
+
+        is_visible = False
+        visibility_start = None
+
+        while current_time <= end_time:
+            astro_time = Time(current_time)
+            
+            error_code, teme_p, teme_v = satellite.sgp4(astro_time.jd1, astro_time.jd2)
+            if error_code != 0:
+                raise RuntimeError(SGP4_ERRORS[error_code])
+
+            teme_p = CartesianRepresentation(teme_p * u.km)
+            teme_v = CartesianDifferential(teme_v * u.km / u.s)
+            teme = TEME(teme_p.with_differentials(teme_v), obstime=astro_time)
+
+            itrs_geo = teme.transform_to(ITRS(obstime=astro_time))
+            altaz_frame = AltAz(obstime=astro_time, location=observer_location)
+            satellite_altaz = itrs_geo.transform_to(altaz_frame)
+
+            azimuth = satellite_altaz.az.degree
+            elevation = satellite_altaz.alt.degree
+
+            if (
+                min_elevation <= elevation <= max_elevation
+                and min_azimuth <= azimuth <= max_azimuth
+            ):
+                if not is_visible:
+                    
+                    visibility_start = current_time
+                    is_visible = True
+            else:
+                if is_visible:
+                    
+                    visibility_times.append(VisionTime(Begin=visibility_start.timestamp(), End=current_time.timestamp()))
+                    is_visible = False
+
+            current_time += time_step
+
+        if is_visible:
+            visibility_times.append(VisionTime(Begin=visibility_start.timestamp(), End=end_time.timestamp()))
+
+        return visibility_times
