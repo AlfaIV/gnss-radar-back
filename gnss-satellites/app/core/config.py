5fbdb738e147d5e1ba324804df13d0a2a6b890d9
alfaiv <alfaiv@yandex.ru>
2025-03-29 09:49:04 +0300

    fix bugs

--- gnss-satellites/app/core/config.py
+++ gnss-satellites/app/core/config.py
@@ -23,7 +23,7 @@ class Configs(BaseSettings):
     DATETIME_FORMAT: str = "%Y-%m-%dT%H:%M:%S"
     DATE_FORMAT: str = "%Y-%m-%d"
 
-    TLE_PATH: str = os.path.join(PROJECT_ROOT, "app", "services", "tle")
+    TLE_PATH: str = os.path.join(PROJECT_ROOT, "app", "services", "tle", "gps.tle")
 
     class Config:
         case_sensitive = True
